f7cb423d01136d32d6a94410edddfda274a41bee
DDSRem <1448139087@qq.com>
2022-10-01 14:09:40 +0800

    增加txt格式 log文件

--- app/log_mv.sh
+++ app/log_mv.sh
@@ -2,11 +2,13 @@
 
 asp_log_file=/app/log/ASP.log
 mv_log_name=ASP_$(date +%Y%m%d-%H%M%S).log
+mv_log_name_txt=${mv_log_name}.txt
 mv_log_path=/app/log/$(echo $(date +%Y%m%d))
 
 if [ "$asp_log_file" ]; then
     echo -e "\033[34m归档 logs 文件... \033[0m"
     mkdir -p $mv_log_path
     cp $asp_log_file $mv_log_path/$mv_log_name
+    cp $mv_log_path/$mv_log_name $mv_log_path/$mv_log_name_txt
     echo > $asp_log_file
 fi
